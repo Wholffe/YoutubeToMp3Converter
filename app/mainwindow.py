f78d5e486d5ccda23a1531b1078e4fecb956bfac
Wholffe <marc2002sch@googlemail.com>
2025-02-21 00:14:41 +0100

    add stylesheet, add format and quality property, add menubar

--- app/mainwindow.py
+++ app/mainwindow.py
@@ -7,6 +7,10 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("app/ui/mainWindow.ui", self)
+        with open("app/styles.css","r") as f:
+            self.setStyleSheet(f.read())
+        self.comboBoxFormat.addItems(["MP4", "MP3", "WebM"])
+        self.comboBoxQuality.addItems(["Audio Only","1080p", "720p","480p"])
         self.lineEditVideoUrl.textChanged.connect(self.onTextChanged)
         self.buttonDownload.clicked.connect(self.onDownloadClicked)
         self.downloader = Downloader()
