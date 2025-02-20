a99b6bfd82494e1dd1326e49c2a7d5a52c9645e0
Wholffe <marc2002sch@googlemail.com>
2025-02-20 20:34:29 +0100

    edit downloader, ui

--- app/downloader.py
+++ app/downloader.py
@@ -1,19 +1,18 @@
 import yt_dlp
 import os
+from pathlib import Path
 
 class Downloader():
     def __init__(self):
-        ffmpeg_path = os.path.join(os.path.dirname(__file__), '..', 'bin', 'ffmpeg')
+        self.download_path = str(Path.home() / "Downloads/ytdownloader")
         self.ydl_opts = {
             'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-            'ffmpeg_location': ffmpeg_path,
+            'outtmpl': os.path.join(self.download_path, '%(title)s.%(ext)s'),
         }
 
     def download(self, url, path):
-        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download([url])
+        try:
+            with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                ydl.download([url])
+        except Exception as e:
+            print(e)
--- app/mainwindow.py
+++ app/mainwindow.py
@@ -9,14 +9,11 @@ class MainWindow(QMainWindow):
         uic.loadUi("app/ui/mainWindow.ui", self)
         self.lineEditVideoUrl.textChanged.connect(self.onTextChanged)
         self.buttonDownload.clicked.connect(self.onDownloadClicked)
-
         self.downloader = Downloader()
-        self.downloadPath = "Downloads" #TODO Dynamic path
 
     def onTextChanged(self, text):
         self.buttonDownload.setEnabled(len(text) > 0)
-        print(text)
     
     def onDownloadClicked(self):
-        print("start Downloading")
-        self.downloader.download(self.lineEditVideoUrl.text(), self.downloadPath)
+        print(self.downloader.download_path)
+        self.downloader.download(self.lineEditVideoUrl.text(), self.downloader.download_path)
