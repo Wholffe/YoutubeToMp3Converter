2b0362f5037399c0ec630fc78b256a1c08d9bf3f
Wholffe <marc2002sch@googlemail.com>
2025-02-21 16:13:36 +0100

    add progress bar, update log, reduce options (bring later back)

--- app/downloader.py
+++ app/downloader.py
@@ -8,11 +8,33 @@ class Downloader():
         self.ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': os.path.join(self.download_path, '%(title)s.%(ext)s'),
+            'progress_hooks': [self.progress_hook],
+            'noplaylist': True,
         }
+        self.current_download_url = ''
 
-    def download(self, url, path):
+    def progress_hook(self, d):
+        if d['status'] == 'downloading':
+            percent = d.get('percent', 0)
+            self.status_callback(percent)
+
+        elif d['status'] == 'finished':
+            self.status_callback(100, "Download completed")
+
+        elif d['status'] == 'error':
+            error_message = d.get('error', 'Unknown error')
+            self.status_callback(None, error_message)
+
+    def download(self, url, path, status_callback):
+        self.status_callback = status_callback
         try:
             with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                self.status_callback(10)
+                self.current_download_url = url
+                info_dict = ydl.extract_info(url, download=False)
+                video_title = info_dict.get('title', 'Unknown Title')
+                self.status_callback(20)  
+                self.status_callback(None, f"'{video_title}' has been saved to '{self.download_path}'.")
                 ydl.download([url])
         except Exception as e:
-            print(e)
+            self.status_callback(None, f"Error: {str(e)}")
--- app/mainwindow.py
+++ app/mainwindow.py
@@ -8,33 +8,44 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("app/ui/mainWindow.ui", self)
-        with open("app/styles.css","r") as f:
+        with open("app/styles.css", "r") as f:
             self.setStyleSheet(f.read())
-        self.comboBoxFormat.addItems(["MP4", "MP3", "WebM"])
-        self.comboBoxQuality.addItems(["Audio Only","1080p", "720p","480p"])
+        self.comboBoxFormat.addItems(["WebM"])
+        self.comboBoxQuality.addItems(["Audio Only"])
         self.lineEditVideoUrl.textChanged.connect(self.onTextChanged)
         self.lineEditVideoUrl.setFocus()
         self.buttonDownload.clicked.connect(self.onDownloadClicked)
         self.progressBar.hide()
         self.downloader = Downloader()
-        self.appendText(f"Download Path: {self.downloader.download_path}")
+        self.appendText(f"Download path: {self.downloader.download_path}")
+        self.baseUrl = ["https://www.youtube.com/watch?v=", "https://youtu.be/"]
 
     def onTextChanged(self, text):
-        self.buttonDownload.setEnabled(len(text) > 0)
-    
+        ok = any(text.startswith(base) for base in self.baseUrl) and (text != self.downloader.current_download_url)
+        self.buttonDownload.setEnabled(ok)
+
     def onDownloadClicked(self):
-        print(self.downloader.download_path)
-        self.appendText(f"Downloading {self.lineEditVideoUrl.text()}")
+        self.buttonDownload.setEnabled(False)
+        self.appendText(f"Start download ...'{self.lineEditVideoUrl.text()}'")
         self.progressBar.show()
-        self.downloader.download(self.lineEditVideoUrl.text(), self.downloader.download_path)
-        self.appendText(f"File Content {self.downloader.download_path}", "green")
-    
+        self.downloader.download(self.lineEditVideoUrl.text(), self.downloader.download_path, self.update_progress)
+
+    def update_progress(self, percent, message=None):
+        if message and "Error" in message:
+            self.progressBar.setValue(0)
+            self.progressBar.hide()
+            self.appendText(f"Error: {message}", "red")
+        elif percent is not None:
+            self.progressBar.setValue(percent)
+        elif message:
+            self.appendText(message, "green")
+
     def appendText(self, text, color=''):
         match color:
             case 'red':
-                self.textEditLog.setTextColor(QColor(255,0,0))
+                self.textEditLog.setTextColor(QColor(255, 0, 0))
             case 'green':
-                self.textEditLog.setTextColor(QColor(0,255,0))
+                self.textEditLog.setTextColor(QColor(0, 255, 0))
             case _:
                 self.textEditLog.setTextColor(self.textEditLog.palette().text().color())
         self.textEditLog.append(text)
